84e6750230add5f094df0921c295686808f73dc3
Steve Hurst <shurst@mapaction.org>
2022-10-15 20:49:20 +0100

    Handle multiple files

--- README.md
+++ README.md
@@ -2,10 +2,53 @@
 
 Extract zipped data from Geofabrik, sort and rename into MapActions naming conventions and folder structure
 
-## Instructions
+## Usage
+
+### For Zip Files
 - Go to the [Geofabrik Download website](https://download.geofabrik.de)
 - Navigate to the country page of interest, and download the zipped shapefile. For example, for [Austria](https://download.geofabrik.de/europe/austria.html), download austria-latest-free.shp.zip
 
-Usage: ``python3 geofabrik_to_mapaction.py path/to/your/zipfile.zip``
+Usage: ``python3 geofabrik-extractor.py --zip path/to/your/zipfile.zip``
 
-For example, ``python3 geofabrik_to_mapaction.py /Users/cate/git/data/country_data/sudan/sudan-latest-free.zip`` results in folder structure containing renamed shapefiles as below:
+For example:
+   ``python3 geofabrik-extractor.py --zip /Users/cate/git/data/country_data/sudan/sudan-latest-free.zip`` 
+   
+This would produce a folder named `/processed` in folder structure containing renamed shapefiles as below:
+
+``
+    /Users/cate/git/data/country_data/sudan/processed/206_bldg/sdn_bldg_bdg_py_s0_osm_pp_buildings.shp
+    /Users/cate/git/data/country_data/sudan/processed/218_land/sdn_land_lnd_py_s0_osm_pp_landuse.shp
+    /Users/cate/git/data/country_data/sudan/processed/221_phys/sdn_phys_lak_py_s0_osm_pp_waterbodies.shp
+    /Users/cate/git/data/country_data/sudan/processed/221_phys/sdn_phys_nat_pt_s0_osm_pp_natural.shp
+    /Users/cate/git/data/country_data/sudan/processed/221_phys/sdn_phys_nat_py_s0_osm_pp_natural.shp
+    /Users/cate/git/data/country_data/sudan/processed/221_phys/sdn_phys_riv_ln_s0_osm_pp_rivers.shp
+    /Users/cate/git/data/country_data/sudan/processed/222_pois/sdn_pois_poi_pt_s0_osm_pp_pointsofinterest.shp
+    /Users/cate/git/data/country_data/sudan/processed/222_pois/sdn_pois_poi_py_s0_osm_pp_pointsofinterest.shp
+    /Users/cate/git/data/country_data/sudan/processed/222_pois/sdn_pois_rel_pt_s0_osm_pp_placeofworship.shp
+    /Users/cate/git/data/country_data/sudan/processed/222_pois/sdn_pois_rel_py_s0_osm_pp_placeofworship.shp
+    /Users/cate/git/data/country_data/sudan/processed/229_stle/sdn_stle_stl_pt_s0_osm_pp_settlements.shp
+    /Users/cate/git/data/country_data/sudan/processed/229_stle/sdn_stle_stl_py_s0_osm_pp_settlements.shp
+    /Users/cate/git/data/country_data/sudan/processed/232_tran/sdn_tran_rds_ln_s0_osm_pp_roads.shp
+    /Users/cate/git/data/country_data/sudan/processed/232_tran/sdn_tran_rrd_ln_s0_osm_pp_railways.shp
+    /Users/cate/git/data/country_data/sudan/processed/232_tran/sdn_tran_trf_pt_s0_osm_pp_traffic.shp
+    /Users/cate/git/data/country_data/sudan/processed/232_tran/sdn_tran_trf_py_s0_osm_pp_traffic.shp
+    /Users/cate/git/data/country_data/sudan/processed/232_tran/sdn_tran_trn_pt_s0_osm_pp_transport.shp
+    /Users/cate/git/data/country_data/sudan/processed/232_tran/sdn_tran_trn_py_s0_osm_pp_transport.shp
+``
+
+### For Zip File and Output Folder
+
+For a pre-downloaded `zip` file, the output folder can optionally be specified with the `--outdir` parameter:
+
+Usage: ``python3 geofabrik-extractor.py --zip path/to/your/zipfile.zip --outdir /path/to/output/folder``
+
+For example:
+   ``python3 geofabrik-extractor.py --zip /Users/cate/git/data/country_data/sudan/sudan-latest-free.zip --outdir /tmp`` 
+
+This would produce a folder named `/processed` under the folder specified by the `--outdir` parameter:
+
+``
+    /tmp/processed/206_bldg/sdn_bldg_bdg_py_s0_osm_pp_buildings.shp
+``
+
+...
--- src/geofabrik-extractor.py
+++ src/geofabrik-extractor.py
@@ -2,171 +2,11 @@ import os
 import shutil
 import zipfile
 from pathlib import Path
+import urllib.request, json 
 import ntpath
 import argparse
 import warnings
 import re
-
-# Dictionary of country names and abbreviations
-countries = {'afghanistan': 'afg',
-             'angola': 'ago',
-             'albania': 'alb',
-             'united-arab-emirates': 'are',
-             'argentina': 'arg',
-             'armenia': 'arm',
-             'antigua-and-barbuda': 'atg',
-             'azerbaijan': 'aze',
-             'burundi': 'bdi',
-             'benin': 'ben',
-             'burkina-faso': 'bfa',
-             'bangladesh': 'bgd',
-             'bosnia-and-herzegovina': 'bih',
-             'belize': 'blz',
-             'bolivia': 'bol',
-             'brazil': 'bra',
-             'barbados': 'brb',
-             'bhutan': 'btn',
-             'botswana': 'bwa',
-             'central-african-republic': 'caf',
-             'chile': 'chl',
-             'china': 'chn',
-             'cote-d-ivoire': 'civ',
-             'cameroon': 'cmr',
-             'democratic-republic-of-the-congo': 'cod',
-             'congo': 'cog',
-             'cook-islands': 'cok',
-             'colombia': 'col',
-             'comoros': 'com',
-             'cabo-verde': 'cpv',
-             'costa-rica': 'cri',
-             'cuba': 'cub',
-             'cyprus': 'cyp',
-             'djibouti': 'dji',
-             'dominica': 'dma',
-             'dominican-republic': 'dom',
-             'algeria': 'dza',
-             'ecuador': 'ecu',
-             'egypt': 'egy',
-             'eritrea': 'eri',
-             'ethiopia': 'eth',
-             'fiji': 'fji',
-             'ivory': 'civ',
-             'ivory-coast': 'civ',
-             'isle-of-man': 'imn',
-             'micronesia-(federated-states-of)': 'fsm',
-             'gabon': 'gab',
-             'georgia': 'geo',
-             'ghana': 'gha',
-             'guinea': 'gin',
-             'gambia': 'gmb',
-             'guinea-bissau': 'gnb',
-             'equatorial-guinea': 'gnq',
-             'grenada': 'grd',
-             'guatemala': 'gtm',
-             'guyana': 'guy',
-             'honduras': 'hnd',
-             'croatia': 'hrv',
-             'haiti': 'dom',
-             'indonesia': 'idn',
-             'india': 'ind',
-             'iran-(islamic-republic-of)': 'irn',
-             'iran': 'irn',
-             'iraq': 'irq',
-             'israel': 'isr',
-             'jamaica': 'jam',
-             'jordan': 'jor',
-             'kazakhstan': 'kaz',
-             'kenya': 'ken',
-             'kyrgyzstan': 'kgz',
-             'cambodia': 'khm',
-             'kiribati': 'kir',
-             'saint-kitts-and-nevis': 'kna',
-             'republic-of-korea': 'kor',
-             'kuwait': 'kwt',
-             'lao-peoples-democratic-republic': 'lao',
-             'laos': 'lao',
-             'lebanon': 'lbn',
-             'liberia': 'lbr',
-             'libya': 'lby',
-             'saint-lucia': 'lca',
-             'sri-lanka': 'lka',
-             'lesotho': 'lso',
-             'morocco': 'mar',
-             'republic-of-moldova': 'mda',
-             'madagascar': 'mdg',
-             'maldives': 'mdv',
-             'mexico': 'mex',
-             'marshall-islands': 'mhl',
-             'north-macedonia': 'mkd',
-             'mali': 'mli',
-             'malta': 'mlt',
-             'myanmar': 'mmr',
-             'montenegro': 'mne',
-             'mongolia': 'mng',
-             'mozambique': 'moz',
-             'mauritania': 'mrt',
-             'mauritius': 'mus',
-             'malawi': 'mwi',
-             'malaysia': 'mys',
-             'namibia': 'nam',
-             'niger': 'ner',
-             'nigeria': 'nga',
-             'nicaragua': 'nic',
-             'niue': 'niu',
-             'nepal': 'npl',
-             'nauru': 'nru',
-             'oman': 'omn',
-             'pakistan': 'pak',
-             'panama': 'pan',
-             'peru': 'per',
-             'philippines': 'phl',
-             'palau': 'plw',
-             'papua-new-guinea': 'png',
-             'dem-people-s-rep-of-korea': 'prk',
-             'paraguay': 'pry',
-             'palestina': 'pse',
-             'qatar': 'qat',
-             'romania': 'rou',
-             'rwanda': 'rwa',
-             'saudi-arabia': 'sau',
-             'sudan': 'sdn',
-             'senegal': 'sen',
-             'solomon-islands': 'slb',
-             'sierra-leone': 'sle',
-             'el-salvador': 'slv',
-             'somalia': 'som',
-             'south-sudan': 'ssd',
-             'sao-tome-and-principe': 'stp',
-             'suriname': 'sur',
-             'eswatini': 'swz',
-             'seychelles': 'syc',
-             'syrian-arab-republic': 'syr',
-             'chad': 'tcd',
-             'togo': 'tgo',
-             'thailand': 'tha',
-             'tajikistan': 'tjk',
-             'timor-leste': 'tls',
-             'tonga': 'ton',
-             'trinidad-and-tobago': 'tto',
-             'tunisia': 'tun',
-             'turkey': 'tur',
-             'tuvalu': 'tuv',
-             'united-republic-of-tanzania': 'tza',
-             'uganda': 'uga',
-             'uruguay': 'ury',
-             'uzbekistan': 'uzb',
-             'saint-vincent-and-the-grenadines': 'vct',
-             'venezuela': 'ven',
-             'vietnam': 'vnm',
-             'vanuatu': 'vut',
-             'samoa': 'wsm',
-             'hala-ib-triangle': 'xxx',
-             'ma-tan-al-sarra': 'xxx',
-             'yemen': 'yem',
-             'south-africa': 'zaf',
-             'zambia': 'zmb',
-             'zimbabwe': 'zwe',
-             'haiti-and-domrep': 'dom'}
 
 
 def unzip(src_zip_file, dst_folder):
@@ -225,14 +65,7 @@ def extract_country_name(path_to_zip):
         country_text = file_containing_country_name
         warnings.warn("The expected naming convention, 'country-name-latest-free.shp.zip', was not found. "
                       "This may result in unexpected behaviour.")
-
-    if country_text in countries.keys():
-        return country_text
-    else:
-        raise ValueError("Unable to extract the country name from the filepath. "
-                         f"If the format is correct (e.g., 'country-name-latest-free.shp.zip'), "
-                         f"the location may not have a valid ISO code in our records. "
-                         f"The country detected was {country_text}")
+    return country_text
 
 
 def use_regex(input_text):
@@ -249,15 +82,55 @@ def use_regex(input_text):
     return pattern.match(input_text)
 
 
-if __name__ == "__main__":
+def country_codes_from_geofabrik_country_name(geofabrik_country_name):
+    alpha3_country_codes = []
+    alpha2_country_codes = []
+    with urllib.request.urlopen("https://download.geofabrik.de/index-v1-nogeom.json") as url:
+        data = json.load(url)
+        for feature in data['features']:
+            if (feature['properties']['id'] == geofabrik_country_name): 
+                if (feature['properties'].get("iso3166-1:alpha2", None) is not None):
+                    if type(feature['properties']['iso3166-1:alpha2']) == type([]):
+                        alpha2_country_codes = feature['properties']['iso3166-1:alpha2']
+                    else:
+                        alpha2_country_codes.append(feature['properties']['iso3166-1:alpha2'])
 
+    for alpha2Code in alpha2_country_codes:
+        alpha3_country_codes.append(alpha3code(alpha2Code))
+
+    return (alpha3_country_codes)
+
+def alpha3code(alpha2_country_code):
+    """
+    Returns alpha-3 country code for a given alpha-3 country code
+
+    Inputs:
+        - alpha-2 country code (str):
+    Returns:
+        - alpha-3 code (str)
+    """
+    alpha3Code = None
+    found = False
+    urlPath = "https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.json"
+    with urllib.request.urlopen(urlPath) as url:
+        data = json.load(url)
+
+        for row in data:
+            if (row['alpha-2'] == alpha2_country_code):
+                return row['alpha-3'].lower()
+
+    return (alpha3Code)
+
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Zipped folder downloaded from Geofabrik.")
-    parser.add_argument("zip", type=str, help="Path to a zipped folder downloaded from Geofabrik.")
+    parser.add_argument("-z", "--zip", type=str, help="Zipped folder downloaded from Geofabrik.", default=None)
     parser.add_argument("-o", "--outdir", nargs='?', type=str, help="Folder in which to output the processed data.")
     args = parser.parse_args()
 
-    zip_filepath = args.zip
-    zip_folder = os.path.dirname(zip_filepath)
+    if args.zip is not None:
+        zip_filepath = args.zip
+        zip_folder = os.path.dirname(zip_filepath)
 
     if args.outdir:
         output_root = os.path.join(args.outdir, 'processed')
@@ -279,105 +152,106 @@ if __name__ == "__main__":
 
     country_name = extract_country_name(zip_filepath)
 
-    # find code from dictionary
-    country_code = (countries[country_name])
-    print(" ")
-    print("> Processing: " + country_name + " (" + country_code.upper() + ")")
+    country_codes = country_codes_from_geofabrik_country_name(country_name)
 
-    # Declare file names
-    settlename = country_code + "_stle_stl_pt_s0_osm_pp_settlements"
-    settlepyname = country_code + "_stle_stl_py_s0_osm_pp_settlements"
-    roadname = country_code + "_tran_rds_ln_s0_osm_pp_roads"
-    railname = country_code + "_tran_rrd_ln_s0_osm_pp_railways"
-    traffptname = country_code + "_tran_trf_pt_s0_osm_pp_traffic"
-    traffpyname = country_code + "_tran_trf_py_s0_osm_pp_traffic"
-    transptname = country_code + "_tran_trn_pt_s0_osm_pp_transport"
-    transpyname = country_code + "_tran_trn_py_s0_osm_pp_transport"
-    waterwaysname = country_code + "_phys_riv_ln_s0_osm_pp_rivers"
-    waterbodiesname = country_code + "_phys_lak_py_s0_osm_pp_waterbodies"
-    naturalptname = country_code + "_phys_nat_pt_s0_osm_pp_natural"
-    naturalpyname = country_code + "_phys_nat_py_s0_osm_pp_natural"
-    powptname = country_code + "_pois_rel_pt_s0_osm_pp_placeofworship"
-    powpyname = country_code + "_pois_rel_py_s0_osm_pp_placeofworship"
-    poisptname = country_code + "_pois_poi_pt_s0_osm_pp_pointsofinterest"
-    poispyname = country_code + "_pois_poi_py_s0_osm_pp_pointsofinterest"
-    bldgname = country_code + "_bldg_bdg_py_s0_osm_pp_buildings"
-    landname = country_code + "_land_lnd_py_s0_osm_pp_landuse"
+    for country_code in (country_codes):
+        print(" ")
+        print("> Processing: " + country_name + " (" + country_code.upper() + ") to: " + (output_root.replace('\\', '/')))
 
-    for filepath in extracted_file_list:
-        filename, ext = ntpath.splitext(ntpath.basename(filepath))
+        # Declare file names
+        settlename = country_code + "_stle_stl_pt_s0_osm_pp_settlements"
+        settlepyname = country_code + "_stle_stl_py_s0_osm_pp_settlements"
+        roadname = country_code + "_tran_rds_ln_s0_osm_pp_roads"
+        railname = country_code + "_tran_rrd_ln_s0_osm_pp_railways"
+        traffptname = country_code + "_tran_trf_pt_s0_osm_pp_traffic"
+        traffpyname = country_code + "_tran_trf_py_s0_osm_pp_traffic"
+        transptname = country_code + "_tran_trn_pt_s0_osm_pp_transport"
+        transpyname = country_code + "_tran_trn_py_s0_osm_pp_transport"
+        waterwaysname = country_code + "_phys_riv_ln_s0_osm_pp_rivers"
+        waterbodiesname = country_code + "_phys_lak_py_s0_osm_pp_waterbodies"
+        naturalptname = country_code + "_phys_nat_pt_s0_osm_pp_natural"
+        naturalpyname = country_code + "_phys_nat_py_s0_osm_pp_natural"
+        powptname = country_code + "_pois_rel_pt_s0_osm_pp_placeofworship"
+        powpyname = country_code + "_pois_rel_py_s0_osm_pp_placeofworship"
+        poisptname = country_code + "_pois_poi_pt_s0_osm_pp_pointsofinterest"
+        poispyname = country_code + "_pois_poi_py_s0_osm_pp_pointsofinterest"
+        bldgname = country_code + "_bldg_bdg_py_s0_osm_pp_buildings"
+        landname = country_code + "_land_lnd_py_s0_osm_pp_landuse"
 
-        if filename == "gis_osm_places_free_1":
-            dst = os.path.join(output_root, "229_stle", settlename + ext)
-            shutil.copy(filepath, dst)
+        for filepath in extracted_file_list:
+            filename, ext = ntpath.splitext(ntpath.basename(filepath))
 
-        elif filename == "gis_osm_places_a_free_1":
-            dst = os.path.join(output_root, "229_stle", settlepyname + ext)
-            shutil.copy(filepath, dst)
+            if filename == "gis_osm_places_free_1":
+                dst = os.path.join(output_root, "229_stle", settlename + ext)
+                shutil.copy(filepath, dst)
 
-        elif filename == "gis_osm_railways_free_1":
-            dst = os.path.join(output_root, "232_tran", railname + ext)
-            shutil.copy(filepath, dst)
+            elif filename == "gis_osm_places_a_free_1":
+                dst = os.path.join(output_root, "229_stle", settlepyname + ext)
+                shutil.copy(filepath, dst)
 
-        elif filename == "gis_osm_roads_free_1":
-            dst = os.path.join(output_root, "232_tran", roadname + ext)
-            shutil.copy(filepath, dst)
+            elif filename == "gis_osm_railways_free_1":
+                dst = os.path.join(output_root, "232_tran", railname + ext)
+                shutil.copy(filepath, dst)
 
-        elif filename == "gis_osm_traffic_free_1":
-            dst = os.path.join(output_root, "232_tran", traffptname + ext)
-            shutil.copy(filepath, dst)
+            elif filename == "gis_osm_roads_free_1":
+                dst = os.path.join(output_root, "232_tran", roadname + ext)
+                shutil.copy(filepath, dst)
 
-        elif filename == "gis_osm_traffic_a_free_1":
-            dst = os.path.join(output_root, "232_tran", traffpyname + ext)
-            shutil.copy(filepath, dst)
+            elif filename == "gis_osm_traffic_free_1":
+                dst = os.path.join(output_root, "232_tran", traffptname + ext)
+                shutil.copy(filepath, dst)
 
-        elif filename == "gis_osm_transport_free_1":
-            dst = os.path.join(output_root, "232_tran", transptname + ext)
-            shutil.copy(filepath, dst)
+            elif filename == "gis_osm_traffic_a_free_1":
+                dst = os.path.join(output_root, "232_tran", traffpyname + ext)
+                shutil.copy(filepath, dst)
 
-        elif filename == "gis_osm_transport_a_free_1":
-            dst = os.path.join(output_root, "232_tran", transpyname + ext)
-            shutil.copy(filepath, dst)
+            elif filename == "gis_osm_transport_free_1":
+                dst = os.path.join(output_root, "232_tran", transptname + ext)
+                shutil.copy(filepath, dst)
 
-        elif filename == "gis_osm_water_a_free_1":
-            dst = os.path.join(output_root, "221_phys", waterbodiesname + ext)
-            shutil.copy(filepath, dst)
+            elif filename == "gis_osm_transport_a_free_1":
+                dst = os.path.join(output_root, "232_tran", transpyname + ext)
+                shutil.copy(filepath, dst)
 
-        elif filename == "gis_osm_waterways_free_1":
-            dst = os.path.join(output_root, "221_phys", waterwaysname + ext)
-            shutil.copy(filepath, dst)
+            elif filename == "gis_osm_water_a_free_1":
+                dst = os.path.join(output_root, "221_phys", waterbodiesname + ext)
+                shutil.copy(filepath, dst)
 
-        elif filename == "gis_osm_natural_free_1":
-            dst = os.path.join(output_root, "221_phys", naturalptname + ext)
-            shutil.copy(filepath, dst)
+            elif filename == "gis_osm_waterways_free_1":
+                dst = os.path.join(output_root, "221_phys", waterwaysname + ext)
+                shutil.copy(filepath, dst)
 
-        elif filename == "gis_osm_natural_a_free_1":
-            dst = os.path.join(output_root, "221_phys", naturalpyname + ext)
-            shutil.copy(filepath, dst)
+            elif filename == "gis_osm_natural_free_1":
+                dst = os.path.join(output_root, "221_phys", naturalptname + ext)
+                shutil.copy(filepath, dst)
 
-        elif filename == "gis_osm_pofw_free_1":
-            dst = os.path.join(output_root, "222_pois", powptname + ext)
-            shutil.copy(filepath, dst)
+            elif filename == "gis_osm_natural_a_free_1":
+                dst = os.path.join(output_root, "221_phys", naturalpyname + ext)
+                shutil.copy(filepath, dst)
 
-        elif filename == "gis_osm_pofw_a_free_1":
-            dst = os.path.join(output_root, "222_pois", powpyname + ext)
-            shutil.copy(filepath, dst)
+            elif filename == "gis_osm_pofw_free_1":
+                dst = os.path.join(output_root, "222_pois", powptname + ext)
+                shutil.copy(filepath, dst)
 
-        elif filename == "gis_osm_pois_free_1":
-            dst = os.path.join(output_root, "222_pois", poisptname + ext)
-            shutil.copy(filepath, dst)
+            elif filename == "gis_osm_pofw_a_free_1":
+                dst = os.path.join(output_root, "222_pois", powpyname + ext)
+                shutil.copy(filepath, dst)
 
-        elif filename == "gis_osm_pois_a_free_1":
-            dst = os.path.join(output_root, "222_pois", poispyname + ext)
-            shutil.copy(filepath, dst)
+            elif filename == "gis_osm_pois_free_1":
+                dst = os.path.join(output_root, "222_pois", poisptname + ext)
+                shutil.copy(filepath, dst)
 
-        elif filename == "gis_osm_buildings_a_free_1":
-            dst = os.path.join(output_root, "206_bldg", bldgname + ext)
-            shutil.copy(filepath, dst)
+            elif filename == "gis_osm_pois_a_free_1":
+                dst = os.path.join(output_root, "222_pois", poispyname + ext)
+                shutil.copy(filepath, dst)
 
-        elif filename == "gis_osm_landuse_a_free_1":
-            dst = os.path.join(output_root, "218_land", landname + ext)
-            shutil.copy(filepath, dst)
+            elif filename == "gis_osm_buildings_a_free_1":
+                dst = os.path.join(output_root, "206_bldg", bldgname + ext)
+                shutil.copy(filepath, dst)
+
+            elif filename == "gis_osm_landuse_a_free_1":
+                dst = os.path.join(output_root, "218_land", landname + ext)
+                shutil.copy(filepath, dst)
 
     shutil.rmtree("temp_folder", ignore_errors=False, onerror=None)
     print("> Process complete.")
